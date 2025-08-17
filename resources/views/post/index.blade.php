f5686f7aa9e726fbf4f898f19705e09a2b9c3483
Sebastian <sebastian.kitschke@googlemail.com>
2025-08-17 18:13:20 +0200

    fix(ui): separate mobile toggle and always-visible desktop category menu

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -2,13 +2,20 @@
     <div class="p-4">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-2">
+                {{-- Mobile Menu for Categories --}}
+                <div x-data="{ open: false }" x-on:click="open = ! open" class="sm:hidden flex flex-col justify-center items-center cursor-pointer text-gray-500 p-4">
+                    <div class="text-2xl sm:hidden">Kategorien</div>
+                    <div x-show="open" class="mt-6"><x-category-tabs /></div>
+                </div>
+                {{-- Desktop Menu for Categories --}}
+                <div class="hidden md:flex justify-center p-4">
                     <x-category-tabs />
                 </div>
+
             </div>
             <div class="mt-4 text-gray-900">
                 @forelse($posts as $post)
-                  <x-post-item :post="$post"/>
+                    <x-post-item :post="$post" />
                 @empty
                     <div class="text-center text-gray-400 py-16">Keine Beiträge gefunden</div>
                 @endforelse
