1ecaf5803b08bd2506822da8e68a0f9c017364d4
Sebastian <sebastian.kitschke@googlemail.com>
2025-09-13 16:56:03 +0200

    add trusted proxy for my npm reverse proxy

---
+++ config/trustedproxy.php
@@ -0,0 +1,28 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted Proxies
+    |--------------------------------------------------------------------------
+    | Trage hier deine Proxy-IP(s) ein. Für deinen IONOS NPM:
+    | z.B. '203.0.113.45' (ohne Port). Mehrere als Array.
+    */
+    'proxies' => '85.215.226.9',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted Headers
+    |--------------------------------------------------------------------------
+    | Welche Forwarded-Header Laravel auswertet. "ALL" deckt das Übliche ab.
+    */
+    'headers' =>
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB,
+];
