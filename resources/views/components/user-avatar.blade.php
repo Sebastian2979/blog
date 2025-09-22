6e1e6a7c193e9e834dd8b4ae9ee2cda2a08dfeef
Sebastian <sebastian.kitschke@googlemail.com>
2025-09-22 17:06:56 +0200

    changed avatar default

--- resources/views/components/user-avatar.blade.php
+++ resources/views/components/user-avatar.blade.php
@@ -3,6 +3,6 @@
 @if ($user->imageUrl())
     <img src="{{ $user->imageUrl() }}" alt="{{ $user->name }}" class="{{ $size }} rounded-full object-cover">
 @else
-    <img src="https://liccar.com/wp-content/uploads/png-transparent-head-the-dummy-avatar-man-tie-jacket-user.png"
+    <img src="/avatar.png"
         alt="Dummy" class="{{ $size }} rounded-full object-cover">
 @endif
